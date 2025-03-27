46b21941ca4d1f277997de0055f964ef79768895
AniketSaini0 <yashsaini252000@gmail.com>
2025-03-27 19:22:58 +0530

    add user slice to app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from "react";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState, AppDispatch } from "./redux/store";
+import { fetchAuthStatus } from "./redux/slices/auth.slice";
 import {
   BrowserRouter as Router,
   Routes,
@@ -9,57 +10,22 @@ import {
 } from "react-router-dom";
 
 import Auth from "./Screens/Auth";
-import { checkAuth } from "./services/auth.service";
 import Register from "./Screens/Register";
 import TaskDashboard from "./Screens/TaskDashboard";
-import { Task } from "./types/index";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import "./index.css";
 
 export default function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [loggedUserEmail, setLoggedUserEmail] = useState("");
-
-  const handleLogin = (email: string) => {
-    setLoggedUserEmail(email);
-    setIsAuthenticated(true);
-  };
-  const handleLogout = () => setIsAuthenticated(false);
-
-  const loadTasks = (tasks: Task[]) => setTasks(tasks);
-
-  const addTask = (task: Task) => setTasks([...tasks, task]);
-  const editTask = (updatedTask: Task) =>
-    setTasks(
-      tasks.map((task) => (task.id === updatedTask.id ? updatedTask : task))
-    );
-  const deleteTask = (taskId: string) =>
-    setTasks(tasks.filter((task) => task.id !== taskId));
-  const toggleTaskCompletion = (taskId: string) =>
-    setTasks(
-      tasks.map((task) =>
-        task.id === taskId ? { ...task, completed: !task.isCompleted } : task
-      )
-    );
-
-  const [loading, setLoading] = useState(true); // ✅ New state to track loading
-
-  const checkAuthentication = async () => {
-    const data = await checkAuth();
-    if (data) {
-      setLoggedUserEmail(data?.data?.email);
-      setIsAuthenticated(true);
-    } else {
-      setIsAuthenticated(false);
-    }
-    setLoading(false);
-  };
+  const dispatch: AppDispatch = useDispatch();
+  const { isAuthenticated, loggedUserEmail, loading } = useSelector(
+    (state: RootState) => state.auth
+  );
 
   useEffect(() => {
-    checkAuthentication();
-  }, []);
+    dispatch(fetchAuthStatus()); // Fetch auth state from backend
+  }, [dispatch]);
 
-  // ✅ Show loader while checking authentication
   if (loading) {
     let index = 0;
     return (
@@ -67,7 +33,6 @@ export default function App() {
         <div className="space-y-6">
           <Skeleton height={60} width="100%" borderRadius={8} />
           <Skeleton height={30} width="100%" borderRadius={8} />
-          {/* Skeleton for Multiple Rows */}
           <div className="space-y-4">
             <Skeleton
               key={index++}
@@ -87,13 +52,7 @@ export default function App() {
       <Routes>
         <Route
           path="/auth"
-          element={
-            isAuthenticated ? (
-              <Navigate to="/" />
-            ) : (
-              <Auth onLogin={handleLogin} />
-            )
-          }
+          element={isAuthenticated ? <Navigate to="/" /> : <Auth />}
         />
         <Route
           path="/register"
@@ -103,16 +62,7 @@ export default function App() {
           path="/"
           element={
             isAuthenticated ? (
-              <TaskDashboard
-                tasks={tasks}
-                onLoadTasks={loadTasks}
-                onAddTask={addTask}
-                onEditTask={editTask}
-                onDeleteTask={deleteTask}
-                onToggleTaskCompletion={toggleTaskCompletion}
-                onLogout={handleLogout}
-                userEmail={loggedUserEmail}
-              />
+              <TaskDashboard userEmail={loggedUserEmail!} />
             ) : (
               <Navigate to="/auth" />
             )
