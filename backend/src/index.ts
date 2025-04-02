18be1de0bd6c0fdc082c2966ea98cb7fa82ed978
Aniket Saini <54745064+AniketSaini0@users.noreply.github.com>
2025-04-02 23:53:58 +0530

    updates

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,7 @@ import app from "./app";
 import dotenv from "dotenv";
 
 dotenv.config();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Start Server **AFTER** MongoDB is connected
 connectDB()
