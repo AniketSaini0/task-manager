534573440a077a9a0417fee7e8f8e41d1654eaa0
Aniket Saini <54745064+AniketSaini0@users.noreply.github.com>
2025-04-03 14:45:11 +0530

    modify auth service for redux

--- frontend/src/services/auth.service.ts
+++ frontend/src/services/auth.service.ts
@@ -1,15 +1,23 @@
 export const checkAuth = async () => {
   try {
     const response = await fetch(
-      "http://localhost:8000/api/auth/current-user",
+      "http://localhost:8001/api/auth/current-user",
       {
         method: "GET",
         credentials: "include",
       }
     );
 
-    if (!response.ok) return null;
-    return await response.json();
+    console.log("Raw response:", response);
+
+    if (!response.ok) {
+      throw new Error(`HTTP Error: ${response.status} ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    console.log("checkAuth response:", data.data);
+
+    return data.data || null;
   } catch (error) {
     console.error("Error checking authentication:", error);
     return null;
