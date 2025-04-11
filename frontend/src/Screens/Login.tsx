037f363e96fe289769946fdf4026055d42267178
Aniket Saini <54745064+AniketSaini0@users.noreply.github.com>
2025-04-11 20:12:06 +0530

    add Login dummy Screen

--- frontend/src/Screens/Login.tsx
+++ frontend/src/Screens/Login.tsx
@@ -35,7 +35,7 @@ export default function Login({ onLogin }: LoginProps) {
       console.log("inside handleSubmit");
 
       try {
-        const response = await fetch("http://localhost:8000/api/auth/login", {
+        const response = await fetch("http://localhost:8001/api/auth/login", {
           method: "POST",
           credentials: "include",
           headers: {
